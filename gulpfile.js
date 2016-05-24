0018d9a534881672758d7da3f2f8997670877cd1
Liam Smith <liam.smith@oup.com>
2016-05-24 11:47:58 +0100

    removed hello

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-gulp.task('hello', function() {
-  console.log('Hello Zell');
-});
-
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss') // Gets all files ending with .scss in app/scss
     .pipe(sass())
