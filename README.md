234fb8954562c1c2b9814723406e7b38c2390df6
Liam Smith <liam.smith@oup.com>
2016-05-24 11:34:09 +0100

    gulp starter

---
+++ README.md
@@ -0,0 +1,19 @@
+# gulp-setup
+
+Starter files for gulp.
+
+To install pull down directory and run 
+
+```sh
+$ npm install
+```
+
+Then run
+
+```sh
+$ gulp watch
+```
+
+Then vist http://localhost:3000/
+
+See [gulp getting started](https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md) for more documentation.
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function() {
+  console.log('Hello Zell');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/styles.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload); 
+  gulp.watch('js/**/*.js', browserSync.reload); 
+  // Other watchers
+})
