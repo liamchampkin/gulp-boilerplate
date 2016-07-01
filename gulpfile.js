b11641973f6ca4859ca5abaf120667a6cf0c76c1
Liam Smith <liam.smith@oup.com>
2016-07-01 15:04:55 +0100

    testing out swig

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var swig = require('gulp-swig');
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss') // Gets all files ending with .scss in app/scss
@@ -19,10 +20,19 @@ gulp.task('browserSync', function() {
   })
 })
 
+gulp.task('templates', function(){
+  gulp.src("includes/*.swig")
+    .pipe(swig())
+    .pipe(gulp.dest("site/"));
+});
+
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('scss/**/*.scss', ['sass']); 
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('*.html', browserSync.reload); 
   gulp.watch('js/**/*.js', browserSync.reload); 
+  gulp.watch("includes/*.swig", function(event){
+    gulp.run('templates');
+  });
   // Other watchers
 })
