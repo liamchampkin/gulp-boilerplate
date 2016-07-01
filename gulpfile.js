e8ee82ac2e03ee6568faea3a52dda0cbf50962f8
Liam Smith <liam.smith@oup.com>
2016-07-01 15:47:35 +0100

    seeting up twig

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var twig = require('gulp-twig');
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss') // Gets all files ending with .scss in app/scss
@@ -14,15 +15,23 @@ gulp.task('sass', function() {
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: 'site'
     },
   })
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
+// Compile Twig templates to HTML
+gulp.task('templates', function() {
+    return gulp.src('src/*.html') // run the Twig template parser on all .html files in the "src" directory
+        .pipe(twig())
+        .pipe(gulp.dest('site')); // output the rendered HTML files to the "site" directory
+});
+
+gulp.task('watch', ['browserSync', 'sass', 'templates'], function(){
   gulp.watch('scss/**/*.scss', ['sass']); 
+  gulp.watch('src/*.html', ['templates']); 
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload); 
+  gulp.watch('site/*.html', browserSync.reload); 
   gulp.watch('js/**/*.js', browserSync.reload); 
   // Other watchers
 })
