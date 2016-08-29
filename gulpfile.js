728a75f450862f2468b922c0d33b387ea9ba67ee
Liam Smith <liam.smith@oup.com>
2016-08-29 12:27:58 +0100

    adding autoprefixer uglify and concat to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var twig = require('gulp-twig');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function() {
   return gulp.src('scss/styles.scss') // Gets all files ending with .scss in app/scss
     .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('site/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
+});
+
+gulp.task('js', function() {
+  gulp.src('js/*.js')
+  .pipe(uglify())
+  .pipe(concat('js/scripts.js'))
+  .pipe(gulp.dest('site'))
 });
 
 gulp.task('browserSync', function() {
@@ -27,11 +41,12 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('site')); // output the rendered HTML files to the "site" directory
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'templates'], function(){
+gulp.task('watch', ['browserSync', 'sass', 'templates', 'js'], function(){
   gulp.watch('scss/**/*.scss', ['sass']); 
   gulp.watch('src/**/*.html', ['templates']); 
+  gulp.watch('js/**/*.js', ['js']); 
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('site/*.html', browserSync.reload); 
-  gulp.watch('js/**/*.js', browserSync.reload); 
+  gulp.watch('site/js/**/*.js', browserSync.reload); 
   // Other watchers
 })
